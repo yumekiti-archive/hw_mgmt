d5cb46693e2ec67baf9622ff1c04331e4b29f5f8
hanezumi <kanik1580@gmail.com>
2021-11-12 03:11:30 +0000

    add:Taskテストデータ作成

--- laravel/database/seeds/TaskTableSeeder.php
+++ laravel/database/seeds/TaskTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TaskTableSeeder extends Seeder
 {
@@ -12,5 +13,15 @@ class TaskTableSeeder extends Seeder
     public function run()
     {
         //
+        for($i = 0; $i < 3; $i++){
+            DB::table('tasks')->insert([
+                'lesson_id' => $i,
+                'user_id' => 1,
+                'achievement_count' => 0,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ]);
+        }
+        
     }
 }
