2878e13a13b25b95487343a9ab09956bc53fba8d
yamada2000 <terutanaka.123@gmail.com>
2021-11-12 02:15:21 +0000

    add: Timetableのマイグレーションの作成完了

---
+++ laravel/database/migrations/2021_11_12_021348_create_timetables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->Int('week_count');
+            $table->Int('period');
+            $table->bigint('user_id');
+            $table->bigint('lesson_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
