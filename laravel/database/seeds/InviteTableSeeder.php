494366cdd26fd7e6c0d070b6fa5e1b5af4feeb75
yamada2000 <terutanaka.123@gmail.com>
2021-11-12 03:10:50 +0000

    add: チェックお願いします

---
+++ laravel/database/seeds/InviteTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InviteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('invites')->insert([
+            'user_id' => 1,
+            'invite_user_id' => 2,
+            'token' => 'hogehoge'
+        ]);
+    }
+}
