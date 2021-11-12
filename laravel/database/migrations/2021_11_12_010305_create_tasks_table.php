66852425ad08c06c315c7877ee5b0efd6cc2ab94
hanezumi <kanik1580@gmail.com>
2021-11-12 10:14:57 +0900

    Taskマイグレーションとモデル作成

---
+++ laravel/database/migrations/2021_11_12_010305_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->bigInt('lesson_id');
+            $table->bigInt('user_id');
+            $table->Int('achievement_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
