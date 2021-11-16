a522947fd64ad1f0ca7f0c2855a4897c09430094
behahiii <behahiii@gmail.com>
2021-11-16 04:58:56 +0000

    add: vue

---
+++ vue/src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Test">
+        <li v-for="(todo, key) in todos" :key="key">
+            {{ todo.text }}
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Test',
+        data:function () {
+            return {
+                todos: [
+                    { text: 'Learn JavaScript' },
+                    { text: 'Learn Vue' },
+                    { text: 'Build something awesome' }
+                ]
+            }
+        }
+    }
+</script>
