38485d3d3ec59aef29f4f3fccb56cc7ed3ca25ea
hanezumi <kanik1580@gmail.com>
2021-11-26 02:24:27 +0000

    add:update作成した

--- laravel/database/seeds/UserTableSeeder.php
+++ laravel/database/seeds/UserTableSeeder.php
@@ -14,33 +14,6 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        /*
-        User::factory()->create([
-            'id'=> '1',
-            'name' => 'test1',
-            'email' => 'test1@test.jp',
-            'password' => bcrypt('testtest'),
-            'email' => 'test1@test1.jp',
-            'email_verified_at' =>'test1@1test1.jp', 
-            'timestamps' =>'time1',
-            'rememberToken'=>'resetpassword'
-        ]);
-        DB::table('users')->insert([
-            'id'=> '2',
-            'name' => 'test2',
-            'email' => 'test2@test.jp',
-            'password' => Hash::make('password'),
-            'email' => 'test2@test2.jp',
-            'email_verified_at' =>'test2@2test2.jp', 
-            'timestamps' =>'time2',
-            'rememberToken'=>'resetpassword2'
-        ]);
-        User::factory()->count(8)->create();
-        */
-
-        # 初期化
-        DB::table('users')->delete();
-
         # テストデータ挿入
         DB::table('users')->insert([
             'name'    => 'user1',
