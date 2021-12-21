817ea10af3317a3dea87db9f32e4312a2e0adfc5
山田 妃奈乃 <2200440@MAETEL.ECC.AC.JP>
2021-12-21 16:23:19 +0900

    add: Classマイグレーション作成

---
+++ laravel/app/Schoolclass.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schoolclass extends Model
+{
+    protected $hidden = [
+        'name'
+    ];
+}
---
+++ laravel/database/migrations/2021_12_21_071928_create_schoolclasses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolclassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schoolclasses', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('detail');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schoolclasses');
+    }
+}
