22ca116562c6d5441d0ae577ee0d93cbdc1c98ca
yumekiti <yumekiti120440@gmail.com>
2021-12-28 06:24:29 +0900

    add: label

--- vue/src/components/Rate/Index.vue
+++ vue/src/components/Rate/Index.vue
@@ -24,6 +24,12 @@
                     ></CenterText>
                 </v-col>
             </v-row>
+            <v-row>
+                <v-col cols="12" md="4" class="my-3 d-flex justify-center align-center" v-for="(label, index) in labels" :key="index">
+                    <v-icon class="mb-1 mr-3" :color="label.color">mdi-checkbox-blank-circle</v-icon>
+                    <span>{{label.title}} - {{rate[label.data]}}</span>
+                </v-col>
+            </v-row>
         </v-container>
     </v-card>
 </template>
@@ -42,6 +48,15 @@ export default {
     props: {
         rate: [],
     },
+    data: () => {
+        return {
+            labels: [
+                {title: 'Not Yet', color: 'grey', data: 'not'},
+                {title: 'Done', color: 'blue', data: 'achievement'},
+                {title: 'Today', color: 'green', data: 'today'},
+            ]
+        }
+    }
 }
 </script>
 <style scoped>
