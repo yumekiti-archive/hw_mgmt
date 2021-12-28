64acc0865501cc59d20b2cd5da29184700f28705
yumekiti <yumekiti120440@gmail.com>
2021-12-29 05:08:36 +0900

    add rounded

--- vue/src/components/Task/Index.vue
+++ vue/src/components/Task/Index.vue
@@ -9,7 +9,7 @@
             <v-row>
                 <v-col cols="12" class="d-flex justify-center align-center" v-for="(task, index) in this.tasks" :key="index">
                     <v-card class="card d-flex">
-                        <div class="color"></div>
+                        <div class="color rounded"></div>
                         <v-container class="ml-5 text-h6 text-xl-h4 text-md-h5 d-flex justify-left align-center">
                             {{task.lesson.title}}
                             <v-spacer></v-spacer>
@@ -37,7 +37,7 @@ export default {
 
 .color {
     width: 20px;
-    height: 80%;
+    height: 100%;
     background-color: rgb(23, 109, 185);
 }
 </style>
