5426b746d8490d981651181a11cdfaa242033534
yumekiti <yumekiti120440@gmail.com>
2021-12-31 21:08:51 +0900

    add: Task

--- vue/src/views/Tasks.vue
+++ vue/src/views/Tasks.vue
@@ -5,16 +5,30 @@
             <v-row>
                 <v-col
                     cols="12"
+                    sm="6"
                 >
-                    {{this.task}}
+                    date
+                </v-col>
+                <v-col
+                    cols="12"
+                    sm="6"
+                >
+                    <Task
+                        :tasks="this.task"
+                    ></Task>
                 </v-col>
             </v-row>
         </v-container>
     </div>
 </template>
 <script>
+import Task from '@/components/Task/Index'
+
 export default {
     name: 'Tasks',
+    components: {
+        Task,
+    },
     computed: {
         task(){
             return this.$store.state.task.data
