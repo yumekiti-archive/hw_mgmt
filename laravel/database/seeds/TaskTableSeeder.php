5f416a21e354f9642b8f6275b521654fb97deda4
yumekiti <yumekiti120440@gmail.com>
2022-01-23 23:45:03 +0900

    chenge: task seed

--- laravel/database/seeds/TaskTableSeeder.php
+++ laravel/database/seeds/TaskTableSeeder.php
@@ -14,30 +14,30 @@ class TaskTableSeeder extends Seeder
      */
     public function run()
     {
-        // // 今日のタスク追加
-        // for($i = 1; $i <= 2; $i++){
-        //     $user = User::find($i);
-        //     $timetables = $user->timetables()->get();
-        //     foreach($timetables as $timetable){
-        //         if($timetable->week_count == date('w')){
-        //             $user->tasks()->create([
-        //                 'lesson_id' => $timetable->lesson_id,
-        //                 'user_id' => $user->id,
-        //             ]);
-        //         }
-        //     }
-        // }
-
-        // 今日じゃなくても追加
+        // 今日のタスク追加
         for($i = 1; $i <= 2; $i++){
             $user = User::find($i);
             $timetables = $user->timetables()->get();
             foreach($timetables as $timetable){
-                $user->tasks()->create([
-                    'lesson_id' => $timetable->lesson_id,
-                    'user_id' => $user->id,
-                ]);
+                if($timetable->week_count == date('w')){
+                    $user->tasks()->create([
+                        'lesson_id' => $timetable->lesson_id,
+                        'user_id' => $user->id,
+                    ]);
+                }
             }
         }
+
+        // // 今日じゃなくても追加
+        // for($i = 1; $i <= 2; $i++){
+        //     $user = User::find($i);
+        //     $timetables = $user->timetables()->get();
+        //     foreach($timetables as $timetable){
+        //         $user->tasks()->create([
+        //             'lesson_id' => $timetable->lesson_id,
+        //             'user_id' => $user->id,
+        //         ]);
+        //     }
+        // }
     }
 }
