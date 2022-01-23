57156edf7ad22603f9cec4b91bb4c7e6aff3a985
yumekiti <yumekiti120440@gmail.com>
2022-01-24 01:13:34 +0900

    add: Settings

---
+++ vue/src/components/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <span>Settings</span>
+                </v-col>
+            </v-row>
+            <v-row class="d-flex justify-center align-center" style="text-align: center;">
+                <v-col cols="12">
+                    心が綺麗だとここに色々設定できる画面が見えます。
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'Settings',
+}
+</script>
--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -10,17 +10,22 @@
                         :user="this.user"
                     ></Profile>
                 </v-col>
+                <v-col cols="12">
+                    <Settings></Settings>
+                </v-col>
             </v-row>
         </v-container>
     </div>
 </template>
 <script>
 import Profile from '@/components/Profile'
+import Settings from '@/components/Settings'
 
 export default {
     name: 'UserView',
     components: {
         Profile,
+        Settings,
     },
     computed: {
         user(){
